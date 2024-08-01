0c9c3e0c47e71b4084a956babb5f9416986cc2b8
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2024-08-01 20:11:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from './component/Navigation';
 import Home from './component/home';
-import AddWish from './component/AddWish';
+//import AddWish from './component/AddWish';
 import ProductPage from './component/ProductPage';
 import CategoryPage from './component/CategoryPage';
 import ScrollToTop from './component/ScrollToTop'
@@ -19,7 +19,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/category/:category" element={<CategoryPage />} />
-          <Route path="/add-wish" element={<AddWish />} />
+         // <Route path="/add-wish" element={<AddWish />} />
           <Route path="/add-audio" element={<Question />} />
           <Route path="*" element={<Home />} />
         </Routes>
